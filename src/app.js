83bf7b545952b9d906bb08bd67a0e0d2a358e4c2
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-04-18 03:51:20 +0530

    feat: add healthcheck route and contoller

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+// import all routes
+
+import healthCheckRouter from "./routes/healthcheck.route.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 export { app };
