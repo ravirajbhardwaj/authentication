0df5a3dc215c1db77ba6fd81d1277d640e51eb1a
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-05-02 21:42:46 +0530

    feat: add middleware cors cookieParser urlencoded

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 
-// import all routes
+const corsOptions = {
+  origin:
+    process.env.CORS_ORIGIN === "*" ? "*" : process.env.CORS_ORIGIN?.split(","),
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "12kb" }));
+app.use(express.urlencoded({ extended: true, limit: "12kb" }));
+app.use(cookieParser());
+
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    maxAge: 3600,
+  })
+);
+
+// import all routes
 import healthCheckRouter from "./routes/healthcheck.route.js";
+import UserRouter from "./routes/user.route.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/users", UserRouter);
 
 export { app };
