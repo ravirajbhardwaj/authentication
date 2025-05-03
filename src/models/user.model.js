b08ff8812a2509e92c8c15531a4664e4855426f0
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-05-04 04:09:11 +0530

    refactor: remove jsonwebtoken package

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -7,7 +7,6 @@ import {
 } from "../constants.js";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
-import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -89,30 +88,9 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      role: this.role,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-    }
-  );
-};
+userSchema.methods.generateAccessToken = function () {};
 
-userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    }
-  );
-};
+userSchema.methods.generateRefreshToken = function () {};
 
 userSchema.methods.generateTemporaryToken = function () {
   const unHashedToken = crypto.randomBytes(20).toString("hex");
