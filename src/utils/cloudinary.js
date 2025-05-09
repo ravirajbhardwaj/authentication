f5ca5919bf96313b362a60d1e0b87e623f6c11b1
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-05-09 18:05:43 +0530

    fix: add dotenv to load env properly in cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,6 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 import logger from "../logger/wiston.logger.js";
 import { ApiError } from "../utils/apiError.js";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
