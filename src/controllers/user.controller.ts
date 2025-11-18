d8f8d077ec3eb7ef94b31d2ec5e9419dcf4482b5
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-11-18 23:08:39 +0530

    feat: add user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,77 +1,82 @@
+import { Request, Response } from "express";
 import {
   USER_COOKIE_TOKEN_EXPIRY,
   UserLoginType,
   UserRolesEnum,
-} from "../constants.js";
-import { User } from "../models/user.model.js";
-import { ApiResponse } from "../utils/apiResponse.js";
-import { ApiError } from "../utils/apiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+} from "../constants";
+import { ApiResponse } from "../utils/apiResponse";
+import { ApiError } from "../utils/apiError";
+import asyncHandler from "../utils/asyncHandler";
+
 import {
   emailVerificationMailgenContent,
   forgotPasswordMailgenContent,
   sendMail,
 } from "../utils/mail.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import crypto from "crypto";
-import { generateAccessAndRefreshTokens } from "../services/token.service.js";
+import { uploadOnCloudinary } from "../utils/cloudinary";
+import { generateAccessAndRefreshTokens } from "../services/token.service";
+import { generateTemporaryToken, hashPassword, passwordMatch } from "../utils/helper";
+import { logger } from "../logger/pino.logger";
 
-const registerUser = asyncHandler(async (req, res) => {
+import { prisma } from "../db/database"
+import { User } from "../generated/prisma/client";
+
+export const sanitizeUser = (user: User) => {
+  const {
+    password,
+    emailVerificationToken,
+    emailVerificationExpiry,
+    forgotPasswordToken,
+    forgotPasswordExpiry,
+    createdAt,
+    updatedAt,
+    ...safeUser
+  } = user;
+  return safeUser;
+};
+
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
   // Retrieves the user info from the request body.
   const { fullname, username, email, password, role } = req.body;
 
-  // Validates the user info.
-  if (
-    [fullname, username, email, password, role].some(
-      filed => filed?.trim() === ""
-    )
-  ) {
-    throw new ApiError(400, "All fields are required");
-  }
+  logger.info("Registration attempt" + { email, ip: req.ip });
 
-  if (!(username === username?.toLocaleLowerCase())) {
-    throw new ApiError(400, "Username must be lowercase");
-  }
-
-  // Check if a user with the provided username or email already exists in the database.
-  const existedUser = await User.findOne({
-    $or: [{ username }, { email }],
-  });
+  // Check if a user with the provided email already exists in the database.
+  const existedUser = await prisma.user.findUnique({ where: { email } });
 
   if (existedUser) {
-    throw new ApiError(409, "User with username or email already exists", []);
+    throw new ApiError(409, "User with username or email already exists");
   }
 
-  // Retrieves the user image from the request file.
-  const avatar = req.file;
-  const avatarLocalPath = avatar?.path;
-
-  if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar File is required");
+  let avatarUrl;
+  if (req.file) {
+    try {
+      const uploaded = await uploadOnCloudinary(req.file.path);
+      avatarUrl = uploaded?.secure_url;
+      logger.info("Avatar uploaded successfully" + { email, avatarUrl });
+    } catch (err: any) {
+      logger.warn(`Avatar upload failed for ${email} due to ${err.message}`);
+    }
   }
 
-  const cloudinaryAvatar = await uploadOnCloudinary(avatarLocalPath);
+  // Generate a verification token and its expiry time and HashPassword
+  const hashedPassword = await hashPassword(password);
+  const { unHashedToken, hashedToken, tokenExpiry } = generateTemporaryToken();
 
-  // If the user does not exist, proceed to create a new user
-  const user = await User.create({
-    avatar: cloudinaryAvatar.url,
-    fullname,
-    username,
-    email,
-    password,
-    isEmailVerified: false,
-    role: role ?? UserRolesEnum.USER,
+  // If the user does not exist, create a new user
+  const user = await prisma.user.create({
+    data: {
+      avatar: avatarUrl,
+      fullname,
+      username,
+      email,
+      password: hashedPassword,
+      emailVerificationToken: hashedToken,
+      emailVerificationExpiry: tokenExpiry,
+      isEmailVerified: false,
+      role: role ?? UserRolesEnum.USER,
+    },
   });
-
-  // Generate a verification token and its expiry time
-  const { unHashedToken, hashedToken, tokenExpiry } =
-    user.generateTemporaryToken();
-
-  // Save the generated verification token and its expiry time in the database
-  user.emailVerificationToken = hashedToken;
-  user.emailVerificationExpiry = tokenExpiry;
-
-  await user.save({ validateBeforeSave: false });
 
   // Send a verification email to the user with a link to verify their email address
   await sendMail({
@@ -83,14 +88,15 @@ const registerUser = asyncHandler(async (req, res) => {
     ),
   });
 
-  // Exclude sensitive fields like password and refreshToken from the response for security purposes
-  const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
-  );
+  logger.info("Verification email sent" + { email, userId: user.id, ip: req.ip });
 
-  if (!createdUser) {
-    throw new ApiError(500, "Something went wrong while registering the user");
-  }
+  logger.info("User registered successfully" + {
+    email,
+    userId: user.id,
+    ip: req.ip,
+  });
+
+  const safeUser = sanitizeUser(user);
 
   // Respond with a success message indicating that the user was created successfully.
   return res
@@ -98,19 +104,19 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(
       new ApiResponse(
         201,
-        { user: createdUser },
+        { user: safeUser },
         "Users registered successfully and verification email has been sent on your email"
       )
     );
 });
 
-const loginUser = asyncHandler(async (req, res) => {
+const loginUser = asyncHandler(async (req: Request, res: Response) => {
   // Retrieves the user information from the request body.
   const { username, email, password } = req.body;
 
   // Validates the provided user credentials.
   if (!(username || email)) {
-    throw new ApiError(400, "Please provide either a username or email", []);
+    throw new ApiError(400, "Please provide either a username or email");
   }
 
   if (!password) {
@@ -118,9 +124,7 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 
   // Checks if the user already exists in the database.
-  const user = await User.findOne({
-    $or: [{ username }, { email }],
-  });
+  const user = await prisma.user.findUnique({ where: { email } });
 
   if (!user) {
     throw new ApiError(404, "User does not exist");
@@ -130,15 +134,15 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new ApiError(
       400,
       "You have previously registered using " +
-        user.loginType?.toLowerCase() +
-        ". Please use the " +
-        user.loginType?.toLowerCase() +
-        " login option to access your account."
+      user.loginType?.toLowerCase() +
+      ". Please use the " +
+      user.loginType?.toLowerCase() +
+      " login option to access your account."
     );
   }
 
   // Compares the provided password with the hashed password stored in the database.
-  const isPassowrdMatch = await user.isPasswordCorrect(password);
+  const isPassowrdMatch = await passwordMatch(password, user.password as string);
 
   if (!isPassowrdMatch) {
     throw new ApiError(401, "Invalid user credentials");
@@ -154,10 +158,10 @@ const loginUser = asyncHandler(async (req, res) => {
 
   user.refreshToken = refreshToken;
 
-  await user.save({ validateBeforeSave: false });
+  await prisma.user.save({ validateBeforeSave: false });
 
   // Retrieves the user document, excluding sensitive fields like password and refreshToken.
-  const loggedInUser = await User.findById(user._id).select(
+  const loggedInUser = await prisma.user.findById(user._id).select(
     "-password -emailVerificationToken -emailVerificationExpiry"
   );
 
@@ -182,9 +186,9 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-const logoutUser = asyncHandler(async (req, res) => {
+const logoutUser = asyncHandler(async (req: Request, res: Response) => {
   // A logout request is received from an authenticated user
-  await User.findByIdAndUpdate(
+  await prisma.user.findByIdAndUpdate(
     req.user._id,
     {
       $set: {
@@ -212,7 +216,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged out"));
 });
 
-const verifyEmail = asyncHandler(async (req, res) => {
+const verifyEmail = asyncHandler(async (req: Request, res: Response) => {
   // Retrieves the verification token from the request parameters.
   const verificationToken = req.params.verificationToken;
 
@@ -228,7 +232,7 @@ const verifyEmail = asyncHandler(async (req, res) => {
     .digest("hex");
 
   // Searches for a user associated with the token and its expiry date.
-  const user = await User.findOne({
+  const user = await prisma.user.findOne({
     emailVerificationToken: hashedToken,
     emailVerificationExpiry: { $gt: Date.now() },
   });
@@ -245,7 +249,7 @@ const verifyEmail = asyncHandler(async (req, res) => {
   user.isEmailVerified = true;
 
   // Saves the updated user information to the database.
-  await user.save({ validateBeforeSave: false });
+  await prisma.user.save({ validateBeforeSave: false });
 
   // Respond with a success message indicating the user's email has been successfully verified
   return res
@@ -253,9 +257,9 @@ const verifyEmail = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { isEmailVerified: true }, "Email is verified"));
 });
 
-const resendEmailVerification = asyncHandler(async (req, res) => {
+const resendEmailVerification = asyncHandler(async (req: Request, res: Response) => {
   // A resendEmailVerification request is received from an authenticated user
-  const user = await User.findById(req.user._id);
+  const user = await prisma.user.findById(req.user._id);
 
   // Check if the user's email is already verified
   if (user.isEmailVerified) {
@@ -270,7 +274,7 @@ const resendEmailVerification = asyncHandler(async (req, res) => {
   user.emailVerificationToken = hashedToken;
   user.emailVerificationExpiry = tokenExpiry;
 
-  await user.save({ validateBeforeSave: false });
+  await prisma.user.save({ validateBeforeSave: false });
 
   // Send a verification email to the user with a link to verify their email address
   await sendMail({
@@ -288,48 +292,48 @@ const resendEmailVerification = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Mail has been sent to your mail ID"));
 });
 
-const refreshAccessToken = asyncHandler(async (req, res) => {
-  // Retrieves the authenticated user from the request
-  const user = req.user;
+const refreshAccessToken = asyncHandler(async (req: Request, res: Response) => {
+  // // Retrieves the authenticated user from the request
+  // const user = req.user;
 
-  // Generates new access and refresh tokens for the user
-  const { accessToken, refreshToken: newRefreshToken } =
-    await generateAccessAndRefreshTokens(user._id);
+  // // Generates new access and refresh tokens for the user
+  // const { accessToken, refreshToken: newRefreshToken } =
+  //   await generateAccessAndRefreshTokens(user._id);
 
-  const existedUser = await User.findById(user._id).select(
-    "-password -emailVerificationToken -emailVerificationExpiry"
-  );
+  // const existedUser = await prisma.user.findById(user._id).select(
+  //   "-password -emailVerificationToken -emailVerificationExpiry"
+  // );
 
-  if (!existedUser) {
-    throw new ApiError(404, "User does not exist");
-  }
+  // if (!existedUser) {
+  //   throw new ApiError(404, "User does not exist");
+  // }
 
-  // Updates the user's refresh token in the database
-  existedUser.refreshToken = newRefreshToken;
-  await existedUser.save({ validateBeforeSave: false });
+  // // Updates the user's refresh token in the database
+  // existedUser.refreshToken = newRefreshToken;
+  // await existedUser.save({ validateBeforeSave: false });
 
-  // Sends the access token, refresh token, and user information in the response, setting the tokens as HTTP-only cookies.
-  const options = {
-    httpOnly: true,
-    secure: true,
-    sameSite: "Strict",
-    maxAge: USER_COOKIE_TOKEN_EXPIRY,
-  };
+  // // Sends the access token, refresh token, and user information in the response, setting the tokens as HTTP-only cookies.
+  // const options = {
+  //   httpOnly: true,
+  //   secure: true,
+  //   sameSite: "Strict",
+  //   maxAge: USER_COOKIE_TOKEN_EXPIRY,
+  // };
 
-  return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", newRefreshToken, options)
-    .json(
-      new ApiResponse(
-        200,
-        { accessToken, newRefreshToken },
-        "Access token refreshed"
-      )
-    );
+  // return res
+  //   .status(200)
+  //   .cookie("accessToken", accessToken, options)
+  //   .cookie("refreshToken", newRefreshToken, options)
+  //   .json(
+  //     new ApiResponse(
+  //       200,
+  //       { accessToken, newRefreshToken },
+  //       "Access token refreshed"
+  //     )
+  //   );
 });
 
-const forgotPasswordRequest = asyncHandler(async (req, res) => {
+const forgotPasswordRequest = asyncHandler(async (req: Request, res: Response) => {
   // get email from body
   const { email } = req.body;
 
@@ -338,7 +342,7 @@ const forgotPasswordRequest = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Email is required");
   }
 
-  const user = await User.findOne({ email });
+  const user = await prisma.user.findOne({ email });
 
   if (!user) {
     throw new ApiError(422, "User does not exist");
@@ -351,7 +355,7 @@ const forgotPasswordRequest = asyncHandler(async (req, res) => {
   user.forgotPasswordToken = hashedToken;
   user.forgotPasswordExpiry = tokenExpiry;
 
-  await user.save({ validateBeforeSave: false });
+  await prisma.user.save({ validateBeforeSave: false });
 
   // Send a verification email to the user with a link to verify their email address
   sendMail({
@@ -369,7 +373,7 @@ const forgotPasswordRequest = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Mail has been sent to your mail ID"));
 });
 
-const resetForgottenPassword = asyncHandler(async (req, res) => {
+const resetForgottenPassword = asyncHandler(async (req: Request, res: Response) => {
   // exterct refresh Token from params and get newPasswrod from req body
   const resetToken = req.params.resetToken;
   const { newPassword } = req.body;
@@ -386,7 +390,7 @@ const resetForgottenPassword = asyncHandler(async (req, res) => {
     .digest("hex");
 
   // check token is valid or not expire
-  const user = await User.findOne({
+  const user = await prisma.user.findOne({
     forgotPasswordToken: hashedToken,
     forgotPasswordExpiry: { $gt: Date.now() },
   });
@@ -401,7 +405,7 @@ const resetForgottenPassword = asyncHandler(async (req, res) => {
 
   // set new password and save
   user.password = newPassword;
-  await user.save({ validateBeforeSave: false });
+  await prisma.user.save({ validateBeforeSave: false });
 
   // send success status
   return res
@@ -409,7 +413,7 @@ const resetForgottenPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, {}, "Password reset successfully"));
 });
 
-const changeCurrentPassword = asyncHandler(async (req, res) => {
+const changeCurrentPassword = asyncHandler(async (req: Request, res: Response) => {
   // get data from request body
   const { oldPassword, newPassword } = req.body;
 
@@ -418,7 +422,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Old Password and New Password is required");
   }
 
-  const user = await User.findById(req.user._id);
+  const user = await prisma.user.findById(req.user._id);
 
   // Compares the old password with the hashed password stored in the database.
   const isPassowrdMatch = user.isPasswordCorrect(oldPassword);
@@ -429,7 +433,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
   // update user password to newPassword and save
   user.password = newPassword;
-  await user.save({ validateBeforeSave: false });
+  await prisma.user.save({ validateBeforeSave: false });
 
   // send success status your password is changed
   return res
@@ -437,29 +441,29 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, {}, "Password changed successfully"));
 });
 
-const assignRole = asyncHandler(async (req, res) => {
+const assignRole = asyncHandler(async (req: Request, res: Response) => {
   // get userId from params and role from body
   const userId = req.params.userId;
   const { role } = req.body;
 
   // find user
-  const user = await User.findById(userId);
+  const user = await prisma.user.findById(userId);
 
   if (!user) {
     throw new ApiError(404, "User does not exist");
   }
 
   user.role = role;
-  await user.save({ validateBeforeSave: false });
+  await prisma.user.save({ validateBeforeSave: false });
 
   return res
     .status(200)
     .json(new ApiResponse(200, {}, "Role changed for the user"));
 });
 
-const getCurrentUser = asyncHandler(async (req, res) => {
+const getCurrentUser = asyncHandler(async (req: Request, res: Response) => {
   // Retrieves the currently authenticated user's information from the request object.
-  const user = await User.findById(req.user._id).select(
+  const user = await prisma.user.findById(req.user._id).select(
     "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
   );
 
@@ -469,11 +473,11 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { user }, "Current user fetched successfully"));
 });
 
-const handleSocialLogin = asyncHandler(async (req, res) => {
+const handleSocialLogin = asyncHandler(async (req: Request, res: Response) => {
   //
 });
 
-const updateUserAvatar = asyncHandler(async (req, res) => {
+const updateUserAvatar = asyncHandler(async (req: Request, res: Response) => {
   // Retrieves the user image from the request file.
   const avatar = req.file;
   const avatarLocalPath = avatar?.path;
@@ -487,7 +491,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   const cloudinaryAvatar = await uploadOnCloudinary(avatarLocalPath);
 
   // Find the user in the database by their ID and update the avatar field with the new Cloudinary URL
-  let updatedUser = await User.findByIdAndUpdate(
+  let updatedUser = await prisma.user.findByIdAndUpdate(
     req.user._id,
     {
       $set: {
