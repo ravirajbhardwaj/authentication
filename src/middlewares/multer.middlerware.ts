dbb51600afdabbdaa6d501436bd8d1827e6c47dc
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-11-18 21:33:09 +0530

    feat: add multer middleware

--- src/middlewares/multer.middlerware.ts
+++ src/middlewares/multer.middlerware.ts
@@ -1,6 +1,6 @@
 import multer from "multer";
 import { randomBytes } from "crypto";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/apiError";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
